41b8ec60a074d16ef97ab40b7afa5c2f38898302
naamarahannz <naamarahannz@gmail.com>
2017-12-12 15:35:26 +0300

    adding social media widgets

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',    
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "social_widgets",
+    'django_instagram',
     #'django.contrib.sites',
     'personal',
     'blog',
